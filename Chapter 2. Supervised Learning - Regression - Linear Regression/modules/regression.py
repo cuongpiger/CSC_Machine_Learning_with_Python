3888c106ac6a40af6c251bc06178c89022f8eb3c
Manh Cuong <cuongpigerr@gmail.com>
2021-03-16 11:02:17 +0700

    .

--- Chapter 2. Supervised Learning - Regression - Linear Regression/modules/regression.py
+++ Chapter 2. Supervised Learning - Regression - Linear Regression/modules/regression.py
@@ -1,25 +1,31 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-class MyRegression:
+class MySimpleLinearRegression:
     def __init__(self, data):
         self.data = data
         
     def createTrainTestData(self, X, y, train_size=0.75, random_state=100):
-        X = self.data[X]
+        X = self.data[[X]]
         y = self.data[y]
+        self.X = {}
+        self.y = {}
+
+        self.X['train'], self.X['test'], self.y['train'], self.y['test'] = train_test_split(
+            X, y, random_state=random_state, train_size=0.75)
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=random_state, train_size=0.75)
+    def initModel(self):
+        self.model = LinearRegression()
+        self.model.fit(self.X['train'], self.y['train'])
         
+    def getFormula(self):
         return {
-            'X_train': X_train,
-            'X_test': X_test,
-            'y_train': y_train,
-            'y_test': y_test
+            'intercept': self.model.intercept_,
+            'coef': self.model.coef_[0]
         }
         
-    def simpleLinearRegression(self, X_train, y_train):
-        model = LinearRegression()
-        model.fit(X_train, y_train)
-        
-        return model
+    def predict(self, X=None):
+        if X is None:
+            X = self.X['test']
+            
+        return self.model.predict(X)
