535a436c7db18414e11304be3d83945d7f833e10
Manh Cuong <cuongpigerr@gmail.com>
2021-03-16 12:31:12 +0700

    .

--- Chapter 2. Supervised Learning - Regression - Linear Regression/modules/regression.py
+++ Chapter 2. Supervised Learning - Regression - Linear Regression/modules/regression.py
@@ -1,22 +1,23 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 class MySimpleLinearRegression:
     def __init__(self, data):
         self.data = data
         
     def createTrainTestData(self, X, y, train_size=0.75, random_state=100):
-        X = self.data[[X]]
-        y = self.data[y]
-        self.X = {}
-        self.y = {}
+        self.X = self.data[[X]]
+        self.y = self.data[y]
+        self.train = {}
+        self.test = {}
 
-        self.X['train'], self.X['test'], self.y['train'], self.y['test'] = train_test_split(
-            X, y, random_state=random_state, train_size=0.75)
+        self.train['X'], self.test['X'], self.train['y'], self.test['y'] = train_test_split(
+            self.X, self.y, random_state=random_state, train_size=0.75)
         
     def initModel(self):
         self.model = LinearRegression()
-        self.model.fit(self.X['train'], self.y['train'])
+        self.model.fit(self.train['X'], self.train['y'])
         
     def getFormula(self):
         return {
@@ -26,6 +27,21 @@ class MySimpleLinearRegression:
         
     def predict(self, X=None):
         if X is None:
-            X = self.X['test']
+            X = self.test['X']
             
         return self.model.predict(X)
+    
+    def r2(self, option='all'):
+        if option == 'all':
+            return self.model.score(self.X, self.y)
+        elif option == 'train':
+            return self.model.score(self.train['X'], self.train['y'])
+        else:
+            return self.model.score(self.test['X'], self.test['y'])
+        
+    def leastSquares(self, y_predict, option='mse'):
+        if option == 'mse':
+            return mean_squared_error(y_predict, self.test['y'])
+        elif option == 'mae':
+            return mean_absolute_error(y_predict, self.test['y'])
+            
