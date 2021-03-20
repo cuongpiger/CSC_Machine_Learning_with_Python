2748ba2b9d3ab4723d39579b15d70120ffd3ee2b
Manh Cuong <cuongpigerr@gmail.com>
2021-03-21 01:16:18 +0700

    .

--- Chapter 3. Supervised Learning - Classification - Logistic Regression/modules/logistic.py
+++ Chapter 3. Supervised Learning - Classification - Logistic Regression/modules/logistic.py
@@ -1,7 +1,9 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+from matplotlib.colors import ListedColormap
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -45,20 +47,31 @@ class CLogisticRegression:
             'coef': self.model.coef_
         }
 
-    def visualTestVsPredict(self, x, y):
-        fig, ax = plt.subplots(2, 2, figsize=(15, 15))
-        sns.scatterplot(data=self.X, x=x,
-                        y=y, hue=self.y, ax=ax[0, 0])
-        sns.scatterplot(data=self.X, x=x,
-                        y=y, hue=self.predict(self.X), ax=ax[0, 1])
-        sns.scatterplot(data=self.test['X'], x=x,
-                        y=y, hue=self.test['y'], ax=ax[1, 0])
-        sns.scatterplot(data=self.test['X'], x=x,
-                        y=y, hue=self.predict(self.test['X']), ax=ax[1, 1])
-        ax[0, 0].set_title('Actual Value on Entire Data', fontsize=20)
-        ax[0, 1].set_title('Prediction Value on Entire Data', fontsize=20)
-        ax[0, 1].legend(title=self.test['y'].name)
-        ax[1, 0].set_title('Actual Value on Test Data', fontsize=20)
-        ax[1, 1].set_title('Prediction Value on Test Data', fontsize=20)
-        ax[1, 1].legend(title=self.test['y'].name)
+    def visualTestVsPredict(self, x, y, area=False):
+        if not area:
+            fig, ax = plt.subplots(2, 2, figsize=(15, 15))
+            sns.scatterplot(data=self.X, x=x,
+                            y=y, hue=self.y, ax=ax[0, 0])
+            sns.scatterplot(data=self.X, x=x,
+                            y=y, hue=self.predict(self.X), ax=ax[0, 1])
+            sns.scatterplot(data=self.test['X'], x=x,
+                            y=y, hue=self.test['y'], ax=ax[1, 0])
+            sns.scatterplot(data=self.test['X'], x=x,
+                            y=y, hue=self.predict(self.test['X']), ax=ax[1, 1])
+            ax[0, 0].set_title('Actual Value on Entire Data', fontsize=20)
+            ax[0, 1].set_title('Prediction Value on Entire Data', fontsize=20)
+            ax[0, 1].legend(title=self.test['y'].name)
+            ax[1, 0].set_title('Actual Value on Test Data', fontsize=20)
+            ax[1, 1].set_title('Prediction Value on Test Data', fontsize=20)
+            ax[1, 1].legend(title=self.test['y'].name)
+        else:
+            X1, X2 = np.meshgrid(np.arange(start=self.test['X'][x].min() - 1, stop=self.test['X'][x].max() - 1, step=.01), np.arange(start=self.test['X'][y].min() - 1, stop=self.test['X'][y].max() - 1, step=.01))
+            plt.contourf(X1, X2, self.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha=.75, cmap=ListedColormap(('red', 'green')))
+            plt.xlim(X1.min(), X1.max())
+            plt.ylim(X2.min(), X2.max())
+            plt.scatter(self.test['X'][x], self.test['X'][y], c=self.test['y'], cmap=ListedColormap(('red', 'green')))
+            plt.xlabel(x)
+            plt.ylabel(y)
+            plt.title('Actual Values of Test Data\ndisplay on Prediction Areas Plot', fontsize=20)
+            
         plt.show()
